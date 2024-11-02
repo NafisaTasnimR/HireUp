99c0ad179fd388b820f74294cd62f2ca2afcd556
nafisa <nafisatasnim8855@gmail.com>
2024-11-02 23:45:52 +0600

    Start working on console design

--- HireUP/src/Main.java
+++ HireUP/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Welcome To HIREUP");
+        System.out.println("Do you want to");
+        System.out.println("1.Login");
+        System.out.println("2.Sign up");
+        System.out.println("3.Exit");
+        System.out.println("Enter your selection:");
+
+
+        System.out.println("/////////////////////////////////////////////");
     }
 }
