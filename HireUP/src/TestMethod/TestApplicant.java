2e70650578a39d5606c4b5abd6c53c8939e381fb
nafisa <nafisatasnim8855@gmail.com>
2024-11-04 18:43:57 +0600

    Update searchJob method

--- HireUP/src/TestMethod/TestApplicant.java
+++ HireUP/src/TestMethod/TestApplicant.java
@@ -3,6 +3,8 @@ package TestMethod;
 import HireUpMain.Applicant;
 import org.junit.Test;
 
+import java.util.List;
+
 import static org.junit.Assert.assertEquals;
 
 public class TestApplicant {
@@ -11,7 +13,19 @@ public class TestApplicant {
     public void testsearchJob()
     {
         boolean expectedValue = true;
+        String location = "Uttora";
+        //List<String>
         Applicant applicant = new Applicant();
-        assertEquals(expectedValue,applicant.searchJob());
+
+        assertEquals(expectedValue,applicant.searchJob(location));
+    }
+
+    @Test
+    public void testsearchJob2()
+    {
+        boolean expectedValue = true;
+        String location = "Uttora";
+        Applicant applicant = new Applicant();
+        assertEquals(expectedValue,applicant.searchJob(location));
     }
 }
