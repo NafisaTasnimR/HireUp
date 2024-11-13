5c378f9e268548bfb491ad19f4a9152a154582af
nafisa <nafisatasnim8855@gmail.com>
2024-11-13 12:10:41 +0600

    Implement initial feature of job provider

--- HireUP/src/HireUpMain/JobProvider.java
+++ HireUP/src/HireUpMain/JobProvider.java
@@ -74,4 +74,42 @@ public class JobProvider {
         }
     }
 
+    public void addToShortList(Resume resume)
+    {
+            try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("ApplicantShortList.txt",true)))
+            {
+                bufferedWriter.write(resume.getPersonalInformation().toFileString()+","+
+                        resume.getEducationalInformation().toFileString()+","+
+                        resume.getAdditionalInformation().toFileString());
+                bufferedWriter.newLine();
+            } catch (IOException e)
+            {
+                System.out.println("There is a error : " + e.getMessage());
+            }
+    }
+
+    public void seeShortList()
+    {
+        int serial =0;
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("ApplicantList.txt")))
+        {
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                String[] data = line.split(",");
+                String name = data[0];
+                String phoneNumber = data[7];
+                String email = data[10];
+                serial++;
+                System.out.println(serial + " " + "Name: " + name + " " +
+                       "Phone Number: " + phoneNumber + " " +
+                        "Email: " + email + '\n');
+
+            }
+        }catch (IOException e)
+        {
+            System.out.println("Some error occurred while showing the list: " + e.getMessage());
+        }
+    }
+
 }
