af8174d94ba68f1ef5000a091d5c415229250e4d
nafisa <nafisatasnim8855@gmail.com>
2024-12-12 01:04:02 +0600

    Implement sortList method in main and update sortList method

--- HireUP/src/HireUpMain/JobProvider.java
+++ HireUP/src/HireUpMain/JobProvider.java
@@ -71,11 +71,11 @@ public class JobProvider extends User{
     public List<String> seeApplicantList(String jobPostNo,String sortingPreference)
     {
         List<String> applicantList = new ArrayList<>();
+        int serial = 0;
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader("Application.txt")))
         {
             String line;
             String applicantInfo = null;
-            int serial = 0;
             while((line = bufferedReader.readLine()) != null)
             {
                 String[] data = line.split(",");
@@ -88,11 +88,7 @@ public class JobProvider extends User{
                         {
                             String[] data1 = line1.split(",");
                             if(Objects.equals(data1[10], applicantEmail)){
-                                serial++;
-                                System.out.println(serial + "."+"Name: " + data1[0] +" "+
-                                        "Phone Number: " + data1[7] +" "+
-                                        "Email: " + data1[10] + '\n');
-                                applicantInfo = serial + "," + jobPostNo + "," + line1;
+                                applicantInfo = jobPostNo + "," + line1;
                                 applicantList.add(applicantInfo);
                             }
                         }
@@ -107,6 +103,16 @@ public class JobProvider extends User{
             System.out.println("There is a error : " + e.getMessage());
         }
         this.sortList(applicantList,sortingPreference);
+        for(String applicant: applicantList)
+        {
+            serial++;
+            String[] data1 = applicant.split(",");
+            System.out.println(serial + "."+"Name: " + data1[1] +" "+
+                    "Phone Number: " + data1[8] +" "+
+                    "Email: " + data1[11] + '\n');
+            applicantList.set(serial-1,serial + "," + applicantList.get(serial-1));
+
+        }
         return applicantList;
     }
 
@@ -316,20 +322,20 @@ public class JobProvider extends User{
     {
         if (sortBy.equalsIgnoreCase("cgpa")) {
             applicantList.sort((a, b) -> {
-                double cgpaA = Double.parseDouble(a.split(",")[22]); // Assuming CGPA is the 3rd field in the info
-                double cgpaB = Double.parseDouble(b.split(",")[22]);
+                double cgpaA = Double.parseDouble(a.split(",")[21]); // Assuming CGPA is the 3rd field in the info
+                double cgpaB = Double.parseDouble(b.split(",")[21]);
                 return Double.compare(cgpaB, cgpaA); // Descending order
             });
         } else if (sortBy.equalsIgnoreCase("msc")) {
             applicantList.sort((a, b) -> {
-                String mscA = a.split(",")[23].trim(); // Assuming MSc info is the 5th field in the info
-                String mscB = b.split(",")[23].trim();
+                String mscA = a.split(",")[22].trim(); // Assuming MSc info is the 5th field in the info
+                String mscB = b.split(",")[22].trim();
                 return mscB.compareTo(mscA); // Yes first, No later
             });
         }else if (sortBy.equalsIgnoreCase("experience")) {
             applicantList.sort((a, b) -> {
-                double experienceA = Double.parseDouble(extractYear(a.split(",")[25].trim())); // Assuming MSc info is the 5th field in the info
-                double experienceB = Double.parseDouble(extractYear(b.split(",")[25].trim()));
+                double experienceA = Double.parseDouble(extractYear(a.split(",")[24].trim())); // Assuming MSc info is the 5th field in the info
+                double experienceB = Double.parseDouble(extractYear(b.split(",")[24].trim()));
                 return Double.compare(experienceB, experienceA); // Yes first, No later
             });
         }
