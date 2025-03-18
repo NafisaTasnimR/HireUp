dabd849cbef935e20b4b52bc3fbcf6a1160155ee
NishatTasnimPreownti <preownti@gmail.com>
2025-03-18 12:06:03 +0600

    update viewApplicantDetails in job provider class

--- HireUP/src/HireUpMain/JobProvider.java
+++ HireUP/src/HireUpMain/JobProvider.java
@@ -37,7 +37,7 @@ public class JobProvider extends User{
     }
 
     public static JobProvider loadFromFile(String email) {
-        try (BufferedReader br = new BufferedReader(new FileReader("JobProvider_info.txt"))) {
+        try (BufferedReader br = new BufferedReader(new FileReader("E:\\HireUp\\HireUp\\HireUP\\JobProvider_info.txt"))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] data = line.split(",");
@@ -60,7 +60,7 @@ public class JobProvider extends User{
         String regex = "[,\\.\\s]";
         String[] nameArray = companyName.split(regex);
         String jobPostNo = nameArray[0] + String.valueOf((int)(Math.random()*100));
-        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Job_info.txt", true))) {
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("E:\\HireUp\\HireUp\\HireUP\\Job_info.txt", true))) {
             bufferedWriter.newLine();
             bufferedWriter.write(jobPostNo + "," + formatData(this.getCompanyName()) + "," +
                     formatData(job.getJobPosition()) + "," + formatData(job.getSkill()) + "," + job.getExperience()
@@ -86,7 +86,7 @@ public class JobProvider extends User{
         System.out.println("================================================================================================================");
         List<String> applicantList = new ArrayList<>();
         int serial = 0;
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("Application.txt")))
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("E:\\HireUp\\HireUp\\HireUP\\Application.txt")))
         {
             String line;
             String applicantInfo = null;
@@ -96,7 +96,7 @@ public class JobProvider extends User{
                 String applicantEmail = data[6];
                 if(Objects.equals(this.getEmail(), data[3]) && Objects.equals(jobPostNo, data[0]) && Objects.equals(data[7],"pending"))
                 {
-                    try(BufferedReader bufferedReader1 = new BufferedReader(new FileReader("Applicant_info.txt"))){;
+                    try(BufferedReader bufferedReader1 = new BufferedReader(new FileReader("E:\\HireUp\\HireUp\\HireUP\\Applicant_info.txt"))){;
 
                         String line1;
                         while ((line1 = bufferedReader1.readLine()) != null)
@@ -136,7 +136,7 @@ public class JobProvider extends User{
     {
         String email;
         String applicantResume = null;
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("Applicant_info.txt"))){
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("E:\\HireUp\\HireUp\\HireUP\\Applicant_info.txt"))){
             String line;
             while((line = bufferedReader.readLine()) != null)
             {
@@ -161,39 +161,41 @@ public class JobProvider extends User{
                     }
                 }
                 if(count == 1) {
-                    System.out.println("\n=====================================================");
-                    System.out.println("                       RESUME");
-                    System.out.println("=====================================================\n");
-                    System.out.println("\n-------------   PERSONAL INFORMATION   -------------\n");
-                    System.out.println("Name                         : " + applicantData[0]+ "\n");
-                    System.out.println("Father's Name                : " + applicantData[1]+ "\n");
-                    System.out.println("Mother's Name                : " + applicantData[2]+ "\n");
-                    System.out.println("Date of Birth                : " + applicantData[3]+ "\n");
-                    System.out.println("Nationality                  : " + applicantData[4]+ "\n");
-                    System.out.println("Religion                     : " + applicantData[5]+ "\n");
-                    System.out.println("Gender                       : " + applicantData[6]+ "\n");
-                    System.out.println("Phone Number                 : " + applicantData[7]+ "\n");
-                    System.out.println("Address                      : " + applicantData[8]+ "\n");
-                    System.out.println("National ID                  : " + applicantData[9]+ "\n");
-                    System.out.println("\n------------   EDUCATIONAL INFORMATION   ------------\n");
-                    System.out.println("School Name                  : " + applicantData[11]+ "\n");
-                    System.out.println("Passing Year(SSC/O Level)    : " + applicantData[12]+ "\n");
-                    System.out.println("SSC/O Level Result           : " + applicantData[13]+ "\n");
-                    System.out.println("College Name                 : " + applicantData[14]+ "\n");
-                    System.out.println("Passing Year (HSC/A Level)   : " + applicantData[15]+ "\n");
-                    System.out.println("HSC/A Level Result           : " + applicantData[16]+ "\n");
-                    System.out.println("University Name              : " + applicantData[17]+ "\n");
-                    System.out.println("University Department Name   : " + applicantData[18]+ "\n");
-                    System.out.println("Undergraduate Degree         : " + applicantData[19]+ "\n");
-                    System.out.println("CGPA                         : " + applicantData[20]+ "\n");
-                    System.out.println("Postgraduate Degree          : " + applicantData[21]+ "\n");
-                    System.out.println("CGPA                         : " + applicantData[22]+ "\n");
-                    System.out.println("Extra Educational Information:");
-                    System.out.println("\n------------   ADDITIONAL INFORMATION   ------------\n");
-                    System.out.println("Experience                   : " + applicantData[23]+ "\n");
-                    System.out.println("Hobbies                      : " + applicantData[24]+ "\n");
-                    System.out.println("Skills                       : " + applicantData[25]+ "\n");
-                    System.out.println("=====================================================\n");
+                    System.out.println("\n==========================================================================================================================================");
+                    System.out.println("                                                                 RESUME");
+                    System.out.println("==========================================================================================================================================\n");
+                    System.out.println("\n-------------------------------------------------------   PERSONAL INFORMATION   ---------------------------------------------------------\n");
+                    System.out.println("Name                         : " + applicantData[0] +"\n");
+                    System.out.println("Father's Name                : " + applicantData[1] +"\n");
+                    System.out.println("Mother's Name                : " + applicantData[2] +"\n");
+                    System.out.println("Date of Birth                : " + applicantData[3] +"\n");
+                    System.out.println("Nationality                  : " + applicantData[4] +"\n");
+                    System.out.println("Religion                     : " + applicantData[5] +"\n");
+                    System.out.println("Gender                       : " + applicantData[6] +"\n");
+                    System.out.println("Phone Number                 : " + applicantData[7] +"\n");
+                    System.out.println("Address                      : " + applicantData[8] +"\n");
+                    System.out.println("National ID                  : " + applicantData[9] +"\n");
+                    System.out.println("Email                        : " + applicantData[10] +"\n");
+                    System.out.println("\n-------------------------------------------------------   EDUCATIONAL INFORMATION   ------------------------------------------------------\n");
+                    System.out.println("==========================================================================================================================================");
+                    System.out.println("| Exam Name                  | Institution Name                   | Subject                          | Result       | Passing Year       |");
+                    System.out.println("==========================================================================================================================================");
+
+                    for (int i = 11; i < 41; i += 5) {
+                        if (!applicantData[i].equals("n/a")) {
+                            System.out.printf("| %-26s | %-34s | %-32s | %-12s | %-18s |\n",
+                                    applicantData[i], applicantData[i + 1], applicantData[i + 2], applicantData[i + 3], applicantData[i + 4]);
+                        }
+                    }
+
+                    System.out.println("==========================================================================================================================================");
+                    System.out.println("\n-------------------------------------------------------   ADDITIONAL INFORMATION   -------------------------------------------------------\n");
+                    System.out.println("Experience                   : " + applicantData[41] +"\n");
+                    System.out.println("Hobbies                      : " + applicantData[42] +"\n");
+                    System.out.println("Skills                       : " + applicantData[43] +"\n");
+                    System.out.println("==========================================================================================================================================\n");
+
+
 
                 }
             }
@@ -212,7 +214,7 @@ public class JobProvider extends User{
         String[] data = applicantResume.split(",");
         jobPostNo = data[1];
         applicantEmail = data[12];
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("Application.txt")))
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("E:\\HireUp\\HireUp\\HireUP\\Application.txt")))
         {
             String line;
             while ((line = bufferedReader.readLine()) != null)
@@ -239,7 +241,7 @@ public class JobProvider extends User{
 
     public void addToShortList(List<String> changedApplicationInfo)
     {
-        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("Application.txt"))){
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter("E:\\HireUp\\HireUp\\HireUP\\Application.txt"))){
             for(String application: changedApplicationInfo){
                 bufferedWriter.write(application);
                 bufferedWriter.newLine();
@@ -264,7 +266,7 @@ public class JobProvider extends User{
                 jobPostNo = jobData[1];
             }
         }
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("Application.txt")))
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("E:\\HireUp\\HireUp\\HireUP\\Application.txt")))
         {
             System.out.println("================================================================================================================");
             System.out.println("| S.No | Name                           | Phone Number              | Email                                    |");
@@ -301,7 +303,7 @@ public class JobProvider extends User{
     public List<String> seeJobPosts()
     {
         List<String> jobPostList = new ArrayList<>();
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("Job_info.txt")))
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("E:\\HireUp\\HireUp\\HireUP\\Job_info.txt")))
         {
             System.out.println("================================================================================================================");
             System.out.println("| S.No | Job Post No                    | Company Name              | Position                                 |");
